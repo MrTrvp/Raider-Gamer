3a47a3fc0ef4ba100d045f6159be4bdbf69c2395
Mr. Trvp <marcovquez1995@gmail.com>
2014-11-08 18:51:41 -0800

    Ugly spaces

--- baseClass.php
+++ baseClass.php
@@ -11,15 +11,15 @@
 			if (!isset(self::$games))
 			{
 				self::$games =	[
-									'iMobsters' => new Game('iMobsters', 'kIndl3F1rE158i', 'im', 'a1.63'),
-									'World War' => new Game('World War', 'kIndl3F1rE158w', 'wwar', 'a1.63'),
-									'Kingdoms Live' => new Game('Kingdoms Live', 'D0m1n1c158k', 'kl', 'a1.63'),
-									'Ninjas Live' => new Game('Ninjas Live', '0yAk0D0n157n', 'nl', 'a1.63'),
-									'Pets Live' => new Game('Pets Live', 'pUpp13s157p', 'pl', 'a1.63'),
-									'Racing Live' => new Game('Racing Live', 'g0neIn60s3c158r', 'rl', 'a1.63'),
-									'Vampires Live' => new Game('Vampires Live', 'P4ul158v', 'vl', 'a1.63'),
-									'Zombies Live' => new Game('Zombies Live', 'R0tt1ng157z', 'zl', 'a1.63')
-								];
+							'iMobsters' => new Game('iMobsters', 'kIndl3F1rE158i', 'im', 'a1.63'),
+							'World War' => new Game('World War', 'kIndl3F1rE158w', 'wwar', 'a1.63'),
+							'Kingdoms Live' => new Game('Kingdoms Live', 'D0m1n1c158k', 'kl', 'a1.63'),
+							'Ninjas Live' => new Game('Ninjas Live', '0yAk0D0n157n', 'nl', 'a1.63'),
+							'Pets Live' => new Game('Pets Live', 'pUpp13s157p', 'pl', 'a1.63'),
+							'Racing Live' => new Game('Racing Live', 'g0neIn60s3c158r', 'rl', 'a1.63'),
+							'Vampires Live' => new Game('Vampires Live', 'P4ul158v', 'vl', 'a1.63'),
+							'Zombies Live' => new Game('Zombies Live', 'R0tt1ng157z', 'zl', 'a1.63')
+						];
 			}
 		}
 
